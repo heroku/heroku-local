597f97de7df7085f5e8d6c3ba87f3c45696bd1dd
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-07-08 15:49:05 -0700

    added :start and :run commands

---
+++ commands/run.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let Forego  = require('../lib/forego');
+let cli     = require('heroku-cli-util');
+
+function* run (context) {
+  let forego = new Forego(context.herokuDir);
+  yield forego.ensureSetup();
+  forego.start({args: context.args, flags: context.flags});
+}
+
+module.exports = {
+  topic: 'local',
+  command: 'run',
+  description: 'run heroku app locally',
+  default: true,
+  help: `Start the application specified by a Procfile (defaults to ./Procfile)
+
+Examples:
+
+  heroku local
+  heroku local web
+  heroku local -f Procfile.test -e .env.test`,
+  args: [{name: 'processname', optional: true}],
+  flags: [
+    {name: 'procfile', char: 'f', hasValue: true},
+    {name: 'env', char: 'e', hasValue: true},
+    {name: 'concurrency', char: 'c', hasValue: true},
+    {name: 'port', char: 'p', hasValue: true},
+    {name: 'r', char: 'r', hasValue: false}
+  ],
+  run: cli.command(run)
+};
